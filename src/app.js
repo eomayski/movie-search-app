496fe47fcad2f303ba7d4098e7890af16dce20e7
eomayski <emil@omayski.com>
2025-08-23 12:15:58 +0300

    Movies fetching and

--- src/app.js
+++ src/app.js
@@ -4,14 +4,12 @@
     let searchPage = 1;
 
     const homeBtn = document.getElementById('home-btn');
-    // const searchForm = document.getElementById('search-form');
     const searchBtn = document.getElementById('search-btn');
     const main = document.querySelector('main');
 
     homeBtn.addEventListener('click', goToHome)
     searchBtn.addEventListener('click', goToSearch)
 
-    // searchForm.addEventListener('submit', onSearch)
     goToHome();
 
 
@@ -33,7 +31,7 @@
         main.innerHTML = `
         <div id="search">
             <form id="search-form">
-                <input type="text" id="search-input" placeholder="Search movie title...">
+                <input type="text" name="search" id="search-input" placeholder="Search movie title...">
                 <button type="submit" id="search-icon" class="material-symbols-outlined">search</span>
             </form>
         </div>
@@ -44,8 +42,11 @@
         </div>
         </div>
         `;
-    }
 
+        const searchForm = document.getElementById('search-form');
+        searchForm.addEventListener('submit', onSearch) 
+
+    }
 
     // Function to fetch movies by search term
     async function getMovieBySearch(search) {
@@ -82,6 +83,54 @@
     // Event handler for search form submission
     function onSearch(event) {
         event.preventDefault();
+
+        const formData = new FormData(event.target);
+        const search = formData.get('search').trim();
+
+        if (!search) {
+            return;
+        }
+
+        showAllMovies(search);
+    }
+
+    let movieList = null;
+    async function showAllMovies(search) {
+        movieList = document.getElementById('movies');
+        debugger
+        movieList.innerHTML = "";
+    
+        const movies = await getMovieBySearch(search);
+        movies.forEach(movie => createMovie(movie));
+    }
+
+    function createMovie(movie) {
+        const div = document.createElement("div");
+
+        div.classList.add("movie-card");
+
+        let poster = movie.Poster === "N/A" ? "/images/no-poster.jpg" : movie.Poster;
+
+        div.innerHTML = `
+                <img class="card-img-top" src=${poster} alt="Card image cap" width="400" />
+                    <div class="card-body">
+                    <h4 class="card-title">${movie.Title}</h4>
+                    <a href="details">
+                    </a>
+                    </div>
+                    <div class="card-footer">
+                    <button type="button" data-id=${movie.imdbID} class="btn btn-info">Details</button>
+                    </div>
+        `
+
+        div.querySelector("button").addEventListener("click", showDetails)
+
+
+        movieList.appendChild(div)
+    }
+
+    function showDetails(event) {
+        const id = event.target.dataset.id;    
     }
 
 })();
