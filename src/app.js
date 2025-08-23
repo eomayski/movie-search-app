4b18090c2fc22c1bd60c771e55fd1676ae321c65
eomayski <emil@omayski.com>
2025-08-23 10:42:05 +0300

    Added home view design

--- src/app.js
+++ src/app.js
@@ -3,6 +3,45 @@
     const apiKey = `da16754b`; // If you want to use your own API key, replace this with your key.
     let searchPage = 1;
 
+    const homeBtn = document.getElementById('home-btn');
+    // const searchForm = document.getElementById('search-form');
+    const searchBtn = document.getElementById('search-btn');
+    const main = document.querySelector('main');
+
+    homeBtn.addEventListener('click', goToHome)
+    searchBtn.addEventListener('click', goToSearch)
+
+    // searchForm.addEventListener('submit', onSearch)
+    goToHome();
+
+
+    function goToHome() {
+        document.querySelectorAll('.active').forEach(btn => btn.classList.remove('active'))
+        homeBtn.classList.add('active')
+        main.innerHTML = `
+        <div id="home">
+        <div id="home-title">
+        <h1>Welcome to Movie Search App</h1>
+        <p>Use the search icon above to find your favorite movies!</p>
+        </div>
+        <img src="/images/home.jpg" alt="Movies" id="home-img">
+        </div>`    }
+
+    function goToSearch() {
+        document.querySelectorAll('.active').forEach(btn => btn.classList.remove('active'))
+        searchBtn.classList.add('active')
+        main.innerHTML = `
+        <div id="search">
+            <form id="search-form">
+                <input type="text" id="search-input" placeholder="Search movie title...">
+                <button type="submit" id="search-icon" class="material-symbols-outlined">search</span>
+            </form>
+        </div>
+        <div id="movies"></div>
+        `;
+    }
+
+
     // Function to fetch movies by search term
     async function getMovieBySearch(search) {
         const searchQuery = `/?s=${search}&page=${searchPage}&apikey=`;
@@ -34,7 +73,7 @@
         
     }
 
-    
+
     // Event handler for search form submission
     function onSearch(event) {
         event.preventDefault();
